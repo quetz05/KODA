fd003478eec4b42e0d266f01f5c9613b7ffce448
Jaroslaw <jkornata@hotmail.com>
2015-12-03 22:10:17 +0100

    Super init

---
+++ koda-app/src/main/java/es/elka/koda/app/Application.java
@@ -0,0 +1,10 @@
+package es.elka.koda.app;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Application");
+    }
+}
---
+++ koda-generator/src/main/java/es/elka/koda/generator/Application.java
@@ -0,0 +1,9 @@
+package es.elka.koda.generator;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Generator");
+    }
+}

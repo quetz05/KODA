3f2b608bf2b36e6abbe53472fbc504a9c50aec90
servlok <jakubstrzalek@gmail.com>
2015-12-09 17:03:24 +0100

    Przeniesienie testów do JUnit

---
+++ koda-app/src/test/java/es/elka/koda/app/ApplicationTest.java
@@ -0,0 +1,23 @@
+package es.elka.koda.app;
+
+import es.elka.koda.app.algorithm.HuffmanAlgorithm;
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void shouldDoSomeQuetzThing() {
+        HuffmanAlgorithm algo = new HuffmanAlgorithm();
+
+        algo.addSymbol('a');
+        algo.addSymbol('b');
+        algo.addSymbol('c');
+        algo.addSymbol('d');
+
+        algo.printTree();
+        System.out.println();
+
+        algo.addSymbol('d');
+        algo.printTree();
+    }
+}

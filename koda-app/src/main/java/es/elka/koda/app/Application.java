e759f0f952ea815d1b9235a49ae69d97b6b8c4b2
servlok <jakubstrzalek@gmail.com>
2015-12-14 15:21:48 +0100

    Dodanie obsługi HuffmanAlgorithmService zaimplementowanego przez Quetza

--- koda-app/src/main/java/es/elka/koda/app/Application.java
+++ koda-app/src/main/java/es/elka/koda/app/Application.java
@@ -1,15 +1,20 @@
 package es.elka.koda.app;
 
+import es.elka.koda.app.algorithm.HuffmanAlgorithm;
+import es.elka.koda.app.coder.Coder;
 import es.elka.koda.app.file.EncodedFile;
 import es.elka.koda.app.file.EncodedFileImpl;
 import es.elka.koda.app.file.FileToEncode;
 import es.elka.koda.app.file.PgmFileToEncode;
 
 import java.io.IOException;
+import java.util.BitSet;
 import java.util.List;
 
 public class Application {
     //Console console; coś takiego pewnie tutaj będzie
+
+    private HuffmanAlgorithm huffmanAlgorithm = new HuffmanAlgorithm();
 
     public static void main(String[] args) {
 
@@ -20,14 +25,13 @@ public class Application {
         //TODO pewnie dojdzie gdzie obiekt Console czy coś takiego
     }
 
-    private void code(String path) throws IOException {
+    public void code(String path) throws IOException {
         FileToEncode fileToEncode = new PgmFileToEncode(path);
         List<Byte> dataToEncode = fileToEncode.loadData();
-//         Coder coder = new Coder();
-//         List<BitSet> encodedData = coder.encode();
+        Coder coder = new Coder(huffmanAlgorithm);
+        List<BitSet> encodedData = coder.encode(dataToEncode);
         String fileName = fileToEncode.getPath().getFileName().toString();
         EncodedFile encodedFileToSave = new EncodedFileImpl(fileName);
-//         encodedFileToSave.save();
-
+        encodedFileToSave.save(encodedData);
     }
 }
--- koda-app/src/test/java/es/elka/koda/app/ApplicationTest.java
+++ koda-app/src/test/java/es/elka/koda/app/ApplicationTest.java
@@ -1,7 +1,10 @@
 package es.elka.koda.app;
 
 import es.elka.koda.app.algorithm.HuffmanAlgorithm;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class ApplicationTest {
 
@@ -9,7 +12,21 @@ public class ApplicationTest {
     public void treeAlgorithmTest() {
         HuffmanAlgorithm algo = new HuffmanAlgorithm();
         byte data[] = {1,2,3,4,5,5,5,3,3,3,1,1,2};
-        algo.run(data);
+        algo.run(data, true);
         algo.printTree();
+    }
+
+    /*
+    Moje wlasne testy funkcji
+    Zakladaja ze w okreslonej lokalizacji da się dodać plik
+    stad domyslnie ignorowane
+    */
+    @Test
+    @Ignore
+    public void shouldCodeSourceFile() throws IOException {
+        Application application = new Application();
+
+        application.code("8x8.pgm");
+
     }
 }

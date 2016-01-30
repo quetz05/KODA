cc7795cb59bbc50a6c42f81a2074b095f2b79ca4
servlok <jakubstrzalek@gmail.com>
2016-01-30 16:26:14 +0100

    rozszerzenie generatora o dodatkowe dystrybucje i dlugosc a

--- koda-generator/src/main/java/es/elka/koda/generator/Application.java
+++ koda-generator/src/main/java/es/elka/koda/generator/Application.java
@@ -2,10 +2,14 @@ package es.elka.koda.generator;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.MissingCommandException;
+import es.elka.koda.generator.config.CommandLaplace;
 import es.elka.koda.generator.config.CommandNormal;
+import es.elka.koda.generator.config.CommandUniform;
 import es.elka.koda.generator.config.Commands;
 import org.apache.commons.math3.distribution.AbstractRealDistribution;
+import org.apache.commons.math3.distribution.LaplaceDistribution;
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +27,10 @@ public class Application {
         JCommander jc = new JCommander(new Commands());
         CommandNormal cmdNormal = new CommandNormal();
         jc.addCommand("normal", cmdNormal);
+        CommandLaplace cmdLaplace = new CommandLaplace();
+        jc.addCommand("laplace", cmdLaplace);
+        CommandUniform cmdUniform = new CommandUniform();
+        jc.addCommand("uniform", cmdUniform);
 
         // Commands init end
         try {
@@ -52,13 +60,19 @@ public class Application {
             case "normal":
                 distribution =  new NormalDistribution(cmdNormal.getMean(), cmdNormal.getSd());
                 break;
+            case "laplace":
+                distribution = new LaplaceDistribution(cmdLaplace.getMu(), cmdLaplace.getBeta());
+                break;
+            case "uniform":
+                distribution = new UniformRealDistribution(0.0, 256.0);
+                break;
             default:
                 throw new RuntimeException();
         }
 
         new Generator()
                 .setDistribution(distribution)
-                .generate(128)
+                .generate(commands.getLength())
                 .drawHistogram()
                 .saveFile();
 
